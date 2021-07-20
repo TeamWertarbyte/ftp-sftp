88609729182b001582ebeeffee3f6b076849d860
Johannes Busch <j.busch@wertarbyte.com>
2021-07-20 16:39:32 +0200

    Use readble stream (#1)
    * Add windows line ending support to prettier.
    
    * Return ReadableStream from method readToStream.
    
    * Update Readme.
    
    * Rename method to get a readable stream.
    
    * Return directly readable stream and let callers handle events (e.g. failures).

--- src/local/LocalFileSystem.ts
+++ src/local/LocalFileSystem.ts
@@ -31,13 +31,8 @@ export default class LocalFileSystem implements FileSystem<LocalFileInfo> {
     return readFile(path);
   }
 
-  readToStream(path: string, destination: NodeJS.WritableStream): Promise<void> {
-    const stream = createReadStream(path);
-    return new Promise((resolve, reject) => {
-      stream.once('error', (err) => reject(err));
-      stream.once('end', () => resolve());
-      stream.pipe(destination);
-    });
+  async getStream(path: string): Promise<NodeJS.ReadableStream> {
+    return createReadStream(path);
   }
 
   async mkdir(path: string, recursive: boolean): Promise<void> {
